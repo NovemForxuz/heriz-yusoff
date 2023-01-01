925a5d29d46c3b6d6b55f51b218d23e0f0a2eb9b
Heriz Yusoff <mherizpy@live.com>
2023-01-01 21:35:23 +0800

    Added about content

--- src/components/About.js
+++ src/components/About.js
@@ -5,12 +5,45 @@ const About = (props) => {
 
     return (
         <div>
-            <h3>About</h3>
-            <ul>
-                <li><Link to='/project'>Project</Link></li>
-                <li><Link to='/about'>About</Link></li>
-                <li><Link to='/contact'>Contact</Link></li>
-            </ul>
+            <h1>About</h1>
+            <h3>I'm Heriz. A developer, maker and problem solver.</h3>
+            <p>
+            I’ve been designing with computers since the day I first opened Microsoft Paint. The cusp of art and technology has always fascinated me and I've never been afraid to just jump in and give it a go, whether it's Paint, Photoshop, Sketch or CSS. 
+
+            Fast forward to 2022 and I’ve tried it all, from Digital Campaign Design and Flash Actionscript to Web Design, Animation, HTML/CSS/Webflow, Product Design and Product Management. Everything I have done, small or big, has been a vital stepping stone for where I am today.
+
+            What excites me most about working in software development is being able to design and create things that have purpose and solve real problems. Leaning into customer research and insight, finding the right problems to solve, delivering that value as quickly as possible, learning from it and then iterating and improving that value over time is the key to great design.
+
+            One of my favourite quotes by Peter Drucker:
+            </p>
+            <h3>“There is nothing so useless as doing efficiently that which should not be done at all.”</h3>
+            <p>
+            This resonates so deeply with me because I have no desire to make things just for the sake of it. I only want to make, design or build ideas and products that matter and have a positive impact in the world.
+
+
+            Experience
+
+            - Degree in Information Engineering and Media
+            - Recent experience in Digital Design, Web Development, and Mobile Development
+
+            Within those 14 years, I have:
+
+            - 1 years experience in Software Development
+            - 3 years experience in Front-end Web Development, 
+            - Recent experience in Web Development
+
+
+            <span><h4>Skills</h4></span>
+
+            UI/UX Design / MongoDb / Agile Methodologies / Collaboration / Design Systems / HTML and CSS / Web Development / Responsive Web / Photography 
+            </p>
+            <button>My CV</button>
+            <p>
+            <span><h4>KUDOS</h4></span>
+
+            Most of what is in this portfolio is team work, it's very rare these days to make anything great all on your own. I have only included work that I had a large contribution to, but whether it's design, ideas, UX, process, engineering, mentorship, advice or creative direction, the following people have influenced my growth and career path in a massive way:
+            </p>
+            <button><Link to={'/contact'}>Contact me</Link></button>
         </div>
     )
 }
