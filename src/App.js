8e2e98ed33ce2b5980925a7fe6b26641b08753cd
Heriz Yusoff <mherizpy@live.com>
2023-01-01 17:53:20 +0800

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h4>Hi Heriz,</h4>
+      <p>This will be my online portfolio</p>
     </div>
   );
 }
