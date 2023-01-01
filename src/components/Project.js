cf261c6fba03c7e63287c4f482c6636c3ae4adb0
Heriz Yusoff <mherizpy@live.com>
2023-01-01 19:01:45 +0800

    Added projects content

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,18 +1,46 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { Component } from 'react'
+import { NavLink } from 'react-router-dom'
 
-const Project = (props) => {
+class Project extends Component {
+    state = {
+        projects: [
+            { title: "RISE PROJECT", subtitle: "WEB DEVELOPMENT", id: 1 },
+            { title: "WEB PORTFOLIO", subtitle: "REACT DEVELOPMENT", id: 2 },
+            { title: "FLOAT", subtitle: "REACT NATIVE DEVELOPMENT", id: 3 },
+            { title: "MASHU", subtitle: "MARKETING & WEB DEVELOPMENT", id: 4 },
+            { title: "MOVIE BOOKING", subtitle: "FULLSTACK WEB DEVELOPMENT", id: 5 },
+            { title: "SUDOKU", subtitle: "GAME DEVELOPMENT", id: 6 },
+            { title: "VMS", subtitle: "MOBILE DEVELOPMENT", id: 7 },
+            { title: "QUIZ", subtitle: "MOBILE DEVELOPMENT", id: 8 }
+        ]
+    }
+    
+    render() {
+        const { projects } = this.state
+        const projectList = projects.length ? (
+            projects.map(project => {
+                return (
+                    <div key={project.id}>
+                        <h2>{project.title}</h2>
+                        <h4>- {project.subtitle}</h4>
+                    </div>
+                )
+            })
+        ) : (
+            <div>There are no projects to display</div>
+        )
 
-    return (
-        <div>
-            <h3>Project</h3>
-            <ul>
-                <li><Link to='/project'>Project</Link></li>
-                <li><Link to='/about'>About</Link></li>
-                <li><Link to='/contact'>Contact</Link></li>
-            </ul>
-        </div>
-    )
+        return (
+            <div>
+                <div>
+                    <h4>PROJECTS</h4>
+                    <p>This is a showcase of my best work in a variety of fields, from Fullstack Web development, Marketing and Mobile App development.</p>
+                    <p>The world of Web Technologies has grown at an extremely rapid rate over the last 10 years and my aim has been to evolve with it. I’m learning and gaining new skills every day.</p>
+                </div>
+                {projectList}
+            </div>
+        )
+    }
 }
 
 export default Project
