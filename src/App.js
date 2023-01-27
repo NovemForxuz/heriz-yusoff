f05939f9232f3d77a1698a10b121a264ebdee888
Heriz Yusoff <mherizpy@live.com>
2023-01-28 03:14:47 +0800

    Fixes

--- src/App.js
+++ src/App.js
@@ -4,10 +4,9 @@ import Home from './components/Home'
 import About from './components/About';
 import Contact from './components/Contact';
 import Project from './components/Project';
-import { inject } from '@vercel/analytics'
 
 class App extends Component {
-  inject()
+  
   render() {
     return (
       <BrowserRouter>
