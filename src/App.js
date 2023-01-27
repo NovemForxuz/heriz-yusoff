1afbb18cb34c78acea0f6417083fb89e89aa4bad
Heriz Yusoff <mherizpy@live.com>
2023-01-28 03:10:05 +0800

    Added vercel analytics

--- src/App.js
+++ src/App.js
@@ -4,8 +4,10 @@ import Home from './components/Home'
 import About from './components/About';
 import Contact from './components/Contact';
 import Project from './components/Project';
+import { inject } from '@vercel/analytics'
 
 class App extends Component {
+  inject()
   render() {
     return (
       <BrowserRouter>
