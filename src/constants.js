2ba9b3069ad128e19750aa7bf047bc4df962094a
Heriz Yusoff <mherizpy@live.com>
2026-08-26 23:40:46 +0800

    Fix text

---
+++ src/constants.js
@@ -0,0 +1,19 @@
+// API constants
+
+
+// About
+export const description = {
+    'main-quote' : "I'm Heriz. A developer, problem solver, runner",
+    'main-long' : "I’ve been coding with computers since the day I first learned Object-oriented programming. Solving algorithms has always fascinated me and I’m never afraid to try something new to explore different technologies, be it frontend or backend.\n\nFast forward to 2024 and I’ve tried a bunch, from simple Game pro jects and Mobile Apps to Web Apps using HTML/CSS/JavaScript. Everything I've done, big or small, has been a crucial stepping stone to where I am now. \n\nWhat excites me most about working in software development is being able to code and create things that have purpose and solve real problems. Leaning into customer research and insight, finding the right problems to solve, delivering that value as quickly as possible, learning from it and then iterating and improving that value over time is the key to great software developement.",
+    'coding-short' : 'One of my favourite quotes by Joel Spolsky:',
+    'coding-quote' : 'It is harder to read code than to write it.',
+    'coding-long' : "This resonates so deeply with me because writing good codes has been something I value across my experience with different teams. Having good practises is one of the pillars to a succsesful maintainable codebase that every developer appreciates."
+    
+}
+
+// export const experience = [
+//     {
+//         'type' : '',
+
+//     }
+// ]
